2dca680cb4f73f967fe083b4c6cbe483d2279a7c
thi anh thu <thu.hoang@onemount.com>
2021-09-29 18:45:37 +0700

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,138 @@
+import entities.PhanSo;
+
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+import static entities.PhanSo.parseFromString;
+
+public class App {
+    public static ArrayList<PhanSo> ps = new ArrayList<>();
+
+
+    public static void main(String[] args) {
+        System.out.println("Please select option below:");
+        Scanner sc = new Scanner(System.in);
+        Scanner sc1 = new Scanner(System.in);
+        try {
+            do {
+                System.out.println();
+                System.out.println("1. Input element");
+                System.out.println("2. Print element");
+                System.out.println("3. Add Element");
+                System.out.println("4. Delete Element");
+                System.out.println("5. Update Element");
+                System.out.println("6. Find element which has largest value");
+                System.out.println("7. Exit");
+                System.out.println();
+
+                int command = sc.nextInt();
+                switch (command) {
+                    case 1:
+                        System.out.println("Số lượng phân số cần nhập:");
+                        try {
+                            int n = sc.nextInt();
+                            for (int i = 0; i < n; i++) {
+                                System.out.println("Nhập vào phân số thứ: " + (i + 1));
+
+                                String str = sc1.nextLine();
+                                ps.add(i, PhanSo.parseFromString(str));
+                            }
+                            System.out.println(ps);
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid format input");
+                        }
+                        break;
+                    case 2:
+                        if (ps.size() == 0) {
+                            System.out.println("Danh sách rỗng: ");
+                        } else {
+                            System.out.println("Danh sách các phân số: ");
+                            for (int i = 0; i < ps.size(); i++) {
+                                ps.get(i).print();
+                            }
+                        }
+                        break;
+                    case 3:
+                        System.out.println("Nhập số lượng phân số cần thêm: ");
+                        try {
+                            int e = sc.nextInt();
+                            for (int i = 0; i < e; i++) {
+                                System.out.println("Nhập vào phân số thứ: " + (i + 1));
+                                String str = sc1.nextLine();
+                                ps.add(i, PhanSo.parseFromString(str));
+                            }
+                        } catch (NumberFormatException e) {
+                            System.out.println("Invalid format input");
+                        }
+                        break;
+                    case 4:
+                        if (ps.size() == 0) {
+                            System.out.println("Danh sách rỗng, không thể chọn chức năng này ");
+                        } else {
+                            System.out.println("Mời nhập vị trí phân số cần xóa");
+                            int index = sc.nextInt();
+                            if (index >= ps.size())
+                                System.out.println("Vị trí không tồn tại");
+                            for (int i = 0; i < ps.size(); i++) {
+                                if (i == index) {
+                                    ps.remove(i);
+                                }
+                            }
+                        }
+                        break;
+                    case 5:
+                        if (ps.size() == 0) {
+                            System.out.println("Danh sách rỗng, không thể chọn chức năng này ");
+                        } else {
+                            System.out.println("Mời nhập vị trí phân số cần thay đổi");
+                            int index1 = sc.nextInt();
+                            if (index1 > ps.size())
+                                System.out.println("Vị trí không tồn tại");
+                            for (int i = 0; i < ps.size(); i++) {
+                                if (i == index1) {
+                                    System.out.println("Mời nhập phân số mới: ");
+                                    String str = sc1.nextLine();
+                                    ps.set(i, PhanSo.parseFromString(str));
+                                    System.out.println(ps);
+
+                                }
+                            }
+                        }
+                        break;
+                    case 6:
+                        System.out.println("Chưa làm");
+                        break;
+                    default:
+                        return;
+                }
+            } while (true);
+        } catch (InputMismatchException e) {
+            System.out.println("Invalid format input");
+        }
+    }
+
+
+//    public static void nhapPhanSo() {
+//
+//        System.out.println("Số lượng phân số cần nhập:");
+//        int n = Integer.parseInt(sc.nextLine());
+//        for (int i = 0; i < n; i++) {
+//            try {
+//                System.out.print("Mời nhập ps thứ: " + (i + 1) + " ");
+//                String strPs1 = sc.nextLine();
+//                ps.add(i, parseFromString(strPs1));
+//            } catch (NumberFormatException e) {
+//                System.out.println("Invalid format");
+//            }
+//        }
+//    }
+//
+//    public static void deleteElement() {
+//        System.out.println("Mời nhập vị trí cần xóa");
+//        int n = sc.nextInt();
+//        ps.remove(n);
+//    }
+
+}
+
---
+++ src/main/java/entities/PhanSo.java
@@ -0,0 +1,166 @@
+package entities;
+
+import java.util.Scanner;
+
+public class PhanSo {
+
+    private int tuso;
+    private int mauso;
+
+    public void print() {
+        System.out.format("%d/%d \n", this.tuso, this.mauso);
+    }
+
+    public int getTuso() {
+        return this.tuso;
+    }
+
+    public int getMauso() {
+        return this.mauso;
+    }
+
+    public String getDisplayString() {
+        return String.format("%d/%d", this.tuso, this.mauso);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d/%d", this.tuso, this.mauso);
+    }
+
+    public void input(Scanner sc) {
+        String ps = sc.nextLine();
+
+
+    }
+
+    private static int uocChungLonNhat(int a, int b) {
+        if (a == b)
+            return a;
+
+        if (a > b) {
+            return uocChungLonNhat(a - b, b);
+        }
+
+        return uocChungLonNhat(a, b - a);
+    }
+
+    public void rutGonPhanSo() {
+        int ucln = uocChungLonNhat(Math.abs(this.tuso), Math.abs(this.mauso));
+        this.tuso /= ucln;
+        this.mauso /= ucln;
+
+    }
+
+    public void plus(PhanSo x) {
+        if (this.mauso != x.mauso) {
+            this.tuso = this.tuso * x.mauso + x.tuso * this.mauso;
+            this.mauso = this.mauso * x.mauso;
+        } else {
+            this.tuso = this.tuso + x.tuso;
+        }
+        this.rutGonPhanSo();
+    }
+
+    public static PhanSo sumHaiPhanSo(PhanSo ps1, PhanSo ps2) {
+        PhanSo ret = new PhanSo();
+        if (ps1.mauso != ps2.mauso) {
+            ret.tuso = ((ps1.tuso * ps2.mauso) + (ps1.mauso * ps2.tuso));
+            ret.mauso = (ps1.mauso * ps2.mauso);
+        } else {
+            ret.tuso = ((ps1.tuso * ps2.mauso) + (ps1.mauso * ps2.tuso));
+            ret.mauso = ps2.mauso;
+        }
+        ret.rutGonPhanSo();
+
+        return ret;
+    }
+
+    public void hieuHaiPhanSo(PhanSo x) {
+        if (this.mauso != x.mauso) {
+            this.tuso = ((this.tuso * x.mauso) - (this.mauso * x.tuso));
+            this.mauso = (this.mauso * x.mauso);
+        } else {
+            this.tuso = ((this.tuso * x.mauso) - (this.mauso * x.tuso));
+
+        }
+        this.rutGonPhanSo();
+
+    }
+
+    public void tichHaiPhanSo(PhanSo x) {
+        this.tuso = (this.tuso * x.tuso);
+        this.mauso = (this.mauso * x.mauso);
+        rutGonPhanSo();
+    }
+
+    public void chiaHaiPhanSo(PhanSo x) {
+        this.tuso = this.tuso * x.mauso;
+        this.mauso = x.tuso * this.mauso;
+    }
+
+    public boolean isPhanSoToiGian() {
+        if (uocChungLonNhat(Math.abs(this.tuso), Math.abs(this.mauso)) == 1) {
+            return false;
+        }
+        return true;
+    }
+
+    //quy đồng ps1 so với ps2
+    public void quyDongHaiPhanSo(PhanSo x) {
+        if (this.mauso != x.mauso) {
+            this.tuso = this.tuso * x.mauso;
+            this.mauso = this.mauso * x.mauso;
+            rutGonPhanSo();
+
+        }
+
+    }
+
+    public boolean isPhanSoAm() {
+        if (this.tuso == 0)
+            return false;
+        if (this.tuso < 0 && this.mauso < 0) {
+            return false;
+        }
+        if (this.tuso < 0 || this.mauso < 0)
+            return true;
+        return false;
+    }
+
+    public int soSanhHaiPhanSo(PhanSo x) {
+        if (this.mauso == x.mauso) {
+            if (this.tuso > x.tuso) {
+                return 1; // this > x
+            } else if (this.tuso < x.tuso)
+                return -1; // this < x
+        } else if (this.mauso != x.mauso) {
+            long ts1 = this.tuso * x.mauso;
+            long ts2 = this.mauso * x.tuso;
+            long ms1 = this.mauso * x.mauso;
+            long ms2 = x.mauso * this.mauso;
+            if (ms1 == ms2 && ts1 > ts2) {
+                return 1; // this > x
+            } else
+                return -1; // this < x
+        }
+        return 0; // this = x
+    }
+
+    public static PhanSo parseFromString(String str) {
+        String[] parts = str.split("/");
+        PhanSo ps = new PhanSo();
+        ps.tuso = Integer.parseInt(parts[0]);
+        ps.mauso = Integer.parseInt(parts[1]);
+        return ps;
+    }
+
+//    public PhanSo soSanhPhanSo(PhanSo x) {
+//        if (this.mauso == x.mauso) {
+//            if (this.tuso > x.tuso){
+//                return this.tuso;
+//            }
+//        }
+//        }
+//    }
+}
